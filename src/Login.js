7e22bb7c8846b3f8c2607d7f124e03889419bb39
mahshad <mahshadch1381@gmail.com>
2023-11-28 12:06:39 +0330

    add signup log

--- src/Login.js
+++ src/Login.js
@@ -4,14 +4,23 @@ import './Login.css'; // Make sure to have the correct path
 const Login = () => {
     const [usename, setusename] = useState('');
     const [password, setPassword] = useState('');
-
-    const handleLogin = () => {
+    const [usenames, setusenames] = useState('');
+    const [passwords, setPasswords] = useState('');
+    const [phoneNumber, setPhones] = useState('');
+    const handleLogin_log = () => {
         // Handle login logic here (e.g., send a request to a server)
         console.log('usename:', usename);
         console.log('Password:', password);
+
         // Add your login logic here
     };
-
+    const handleLogin_sign = () => {
+        // Handle login logic here (e.g., send a request to a server)
+        console.log('usenames:', usenames);
+        console.log('Passwords:', passwords);
+        console.log('phoneNumber:', phoneNumber);
+        // Add your login logic here
+    };
     return (
         <div className="main">
             <input type="checkbox" id="chk" aria-hidden="true" />
@@ -19,10 +28,10 @@ const Login = () => {
             <div className="signup">
                 <form>
                     <label htmlFor="chk" aria-hidden="true">Sign up</label>
-                    <input type="text" name="txt" placeholder="User name" required="" />
-                    <input type="phoneNumber" name="phoneNumber" placeholder="phoneNumber" required="" />
-                    <input type="password" name="pswd" placeholder="Password" required="" />
-                    <button>Sign up</button>
+                    <input type="text" name="txt" placeholder="User name" required="" value={usenames} onChange={(e) => setusenames(e.target.value)} />
+                    <input type="phoneNumber" name="phoneNumber" placeholder="phoneNumber" required="" value={passwords} onChange={(e) => setPhones(e.target.value)}/>
+                    <input type="password" name="pswd" placeholder="Password" required="" value={phoneNumber} onChange={(e) => setPasswords(e.target.value)}/>
+                    <button type="button" onClick={handleLogin_sign}>Sign up</button>
                 </form>
             </div>
 
@@ -45,7 +54,7 @@ const Login = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <button type="button" onClick={handleLogin}>Login</button>
+                    <button type="button" onClick={handleLogin_log}>Login</button>
                 </form>
             </div>
         </div>
