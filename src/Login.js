d98a9ea8fd2c72f3ee8acb251a2e73042c7c440f
amirreza ahmadi <ahmadiamirreza81@gmail.com>
2023-12-17 17:37:42 +0330

    Revert "Add some routes"
    This reverts commit 364350896489a261466a2e86f3cb22e163fae474.

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from './../styles/Login.module.css'; 
+import './Login.css'; // Make sure to have the correct path
 import axios from "axios";
 const Login = () => {
     const [usename, setusename] = useState('');
@@ -8,22 +8,29 @@ const Login = () => {
     const [passwords, setPasswords] = useState('');
     const [phoneNumber, setPhones] = useState('');
     const handleLogin_log = () => {
-
+        // Handle login logic here (e.g., send a request to a server)
+        console.log('usename:', usename);
+        console.log('Password:', password);
         axios.post('http://localhost:8088/users/login', {
             phone_number: usename,
             password: password,
           })
           .then(function (response) {
-            localStorage.setItem('token', response.data.token.access_token)
-            window.location.href = '/panel'
-
+            console.log(response);
           })
           .catch(function (error) {
             console.log(error);
           });
+        // console.log('Password:', password);
+
+        // Add your login logic here
     };
     const handleLogin_sign = () => {
-
+        // Handle login logic here (e.g., send a request to a server)
+        console.log('usenames:', usenames);
+        console.log('Passwords:', passwords);
+        console.log('phoneNumber:', phoneNumber);
+        // Add your login logic here
         axios.post('http://localhost:8088/users/register', {
             phone_number: phoneNumber,
             name: usenames,
@@ -31,18 +38,18 @@ const Login = () => {
           })
           .then(function (response) {
             console.log(response);
-        })
+          })
           .catch(function (error) {
             console.log(error);
           });
     };
     return (
-        <div className={styles.first}>
-        <div className={styles.main}>
-            <input type="checkbox" id={styles.chk} aria-hidden="true" />
-            <div className={styles.signup}>
+        <div className="main">
+            <input type="checkbox" id="chk" aria-hidden="true" />
+
+            <div className="signup">
                 <form>
-                    <label htmlFor={styles.chk} aria-hidden="true">Sign up</label>
+                    <label htmlFor="chk" aria-hidden="true">Sign up</label>
                     <input type="text" name="txt" placeholder="User name" required="" value={usenames} onChange={(e) => setusenames(e.target.value)} />
                     <input type="phoneNumber" name="phoneNumber" placeholder="phoneNumber" required="" value={phoneNumber} onChange={(e) => setPhones(e.target.value)}/>
                     <input type="password" name="pswd" placeholder="Password" required="" value={passwords} onChange={(e) => setPasswords(e.target.value)}/>
@@ -50,13 +57,13 @@ const Login = () => {
                 </form>
             </div>
 
-            <div className={styles.login}>
+            <div className="login">
                 <form>
-                    <label htmlFor={styles.chk} aria-hidden="true">Login</label>
+                    <label htmlFor="chk" aria-hidden="true">Login</label>
                     <input
                         type="usename"
                         name="usename"
-                        placeholder="phone number"
+                        placeholder="usename"
                         required=""
                         value={usename}
                         onChange={(e) => setusename(e.target.value)}
@@ -72,7 +79,6 @@ const Login = () => {
                     <button type="button" onClick={handleLogin_log}>Login</button>
                 </form>
             </div>
-        </div>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from "react";
 import ReactDom from 'react-dom/client';
-import Login from "./components/Login";
-import Landing from "./components/Landing";
-import Panel from "./components/Panel";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
 
-export default function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="Panel" element={<Panel />} />
-          <Route path="*" element={<Navigate to="/" />} />
-          
-        </Routes>
-      </BrowserRouter>
-    );
-  }
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App/>
+        <Login/>
     </React.StrictMode>
 )
