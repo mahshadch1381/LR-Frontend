7d3770e5976811996eab4ec0d51df2e9f57345f0
MahshadChabok <81689318+mahshadch1381@users.noreply.github.com>
2023-12-24 17:35:48 +0330

    admin pages style
    new styles for admin pages

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import styles from './../styles/Login.module.css';
+import axios from "axios";
 const AdminLogin = () => {
+    const [usename, setadminusename] = useState('');
+    const [password, setadminPassword] = useState('');
+    const handleLogin_log = () => {
+
+        axios.post('http://localhost:8088/users/login', {
+            phone_number: usename,
+            password: password,
+        })
+            .then(function (response) {
+                localStorage.setItem('token', response.data.token.access_token)
+                window.location.href = '/panel'
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
     return (
-        <div>
-            Admin Login         
+        <div className={styles.first}>
+            <div className={styles.main}>
+                <input  type="checkbox" id={styles.chk} aria-hidden="true" />
+                <div className={styles.signup}>
+                    <form>
+                        <label htmlFor={styles.chk} aria-hidden="true">Admin Login</label>
+                        <input className="myinput"
+                               type="usename"
+                               name="usename"
+                               placeholder="phone number"
+                               required=""
+                               value={usename}
+                               onChange={(e) => setadminusename(e.target.value)}
+                        />
+                        <input className="myinput"
+                               type="password"
+                               name="pswd"
+                               placeholder="Password"
+                               required=""
+                               value={password}
+                               onChange={(e) => setadminPassword(e.target.value)}
+                        />
+                        <button  className="mybutton" type="button" onClick={handleLogin_log}>login</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,9 +1,40 @@
+
 import React from 'react';
+import styles from "../styles/Sidebar.module.css";
+import {Link} from "react-router-dom";
+import styles2 from "../styles/Penelright.module.css";
+
 
 const AdminPanel = () => {
-    return (
+    const itemList = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+
+
+
+   return (
         <div>
-            here is admin panel!!!
+            <div className={styles2.panel_right}>
+            <h1 className={styles2.panel_rightfont}>All laptops we have</h1>
+            <div className={styles.sidebar}>
+                <Link to="/" className={styles.sidebarLink}>
+                    Home
+                </Link>
+                <p className={styles.p_style}>Welcome Admin</p>
+                {/* <p className={styles2.p_style}>Welcome Amirreza Ahmadi</p> */}
+                <button className={styles.mybutton4} >
+                    Logout
+                </button>
+
+            </div>
+                <div style={{ width: '500px', height: '550px', overflowY: 'auto',marginLeft:'350px' }}>
+                    <ul style={{ listStyle: 'none', padding: '0', }}>
+                        {itemList.map((item, index) => (
+                            <li key={index} style={{ padding: '8px', borderBottom: '1px solid #eee' }}>
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+       </div>
         </div>
     );
 };
